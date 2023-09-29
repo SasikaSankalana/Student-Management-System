415f0ace70c4fe2b7c45d4b6aa083b5d4f503905
kelumsri <121974398+kelumsri@users.noreply.github.com>
2023-09-29 23:11:13 +0530

    routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import LandingPage from "./components/Landing/Landingpage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserCreate from "./components/UserHandle/UserCreate";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
   return (
-    <div  >
-      <LandingPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/user" element={<UserCreate />} />
+        <Route path="/dashbord" element={<Dashboard/>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/components/UserHandle/UserCreate.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserCreate() {
+  return (
+    <div>
+      Userhandling
+    </div>
+  )
+}
+
+export default UserCreate
